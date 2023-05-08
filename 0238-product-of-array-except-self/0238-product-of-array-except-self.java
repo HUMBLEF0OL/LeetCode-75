c09d80caddbc14347620b2ecd4615be4b69266fa
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-05-08 19:25:34 +0530

    Time: 2 ms (49.79%), Space: 50.7 MB (74.50%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,37 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int overallProduct = 1;
+        int productOverall = 1;
         int numberOfZeroes = 0;
-        for(int number : nums){
-            if(number == 0){
-                ++numberOfZeroes;
-                if(numberOfZeroes > 1){
-                    overallProduct = 0;
-                } else{
-                    continue;
-                }
-            } else{
-                overallProduct *= number;
+        for(int number:nums) {
+            if(number == 0) {
+                numberOfZeroes++;
+                continue;
             }
+            productOverall *=number;
         }
-        int result[] = new int[nums.length];
-        for(int i =0;i<nums.length;i++){
-            if(overallProduct == 0){
-                result[i] = 0;
-            } else{
-                if(nums[i] == 0){
-                    result[i] = overallProduct;
-                } else{
-                    if(numberOfZeroes>0){
-                        result[i] = 0;
-                    } else{
-                        result[i] = overallProduct/nums[i];   
-                    }
-                    
-                }
-                
-            }
+        for(int i =0;i<nums.length;i++) {
+            if(nums[i] == 0 && numberOfZeroes>1)
+                nums[i] = 0;
+            else if(nums[i] == 0 && numberOfZeroes == 1)
+                nums[i] = productOverall;
+            else if(nums[i]!=0 && numberOfZeroes>=1)
+                nums[i] = 0;
+            else nums[i] = productOverall/nums[i];
         }
-        return result;
+        return nums;
     }
 }
