962f8f742e74f7b242f8f5686377ceed89965420
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-05-10 19:35:59 +0530

    Time: 1 ms (96.75%), Space: 41.6 MB (99.19%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,20 @@
+class Solution {
+    // for the original solution -> look at the first submit(getting error in pushing the value in array,rest was fine)
+    public int compress(char[] chars) {
+        int i = 0, res = 0;
+        while (i < chars.length) {
+            int groupLength = 1;
+            while (i + groupLength < chars.length && chars[i + groupLength] == chars[i]) {
+                groupLength++;
+            }
+            chars[res++] = chars[i];
+            if (groupLength > 1) {
+                for (char c : Integer.toString(groupLength).toCharArray()) {
+                    chars[res++] = c;
+                }
+            }
+            i += groupLength;
+        }
+        return res;
+    }
+}
