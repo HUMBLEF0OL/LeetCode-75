56cc7cb6ace0abee433ccbcfc7b59eaa629ee535
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-05-11 09:29:03 +0530

    Time: 2 ms (44.63%), Space: 44.4 MB (9.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,8 +1,10 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        // here we will be maintain 2 pointers
+		// left and right
+		// if  value at right(i) is not zero then swap it with left and increment both of them by 1
         if(nums.length == 0)return;
         int zeroPointer = 0;
-        int nonZeroPointer = 0;
         for(int i =0;i<nums.length;i++){
             if(nums[i] != 0){
                 int temp = nums[zeroPointer];
