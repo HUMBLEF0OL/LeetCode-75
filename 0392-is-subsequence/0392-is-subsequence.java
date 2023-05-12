f69122024553c25a09517714356337560ced0cb1
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-05-12 07:40:20 +0530

    Time: 4 ms (7.80%), Space: 40.9 MB (24.79%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,7 +3,7 @@ class Solution {
         int indexArr[] = new int[s.length()];
         for(int i =0;i<s.length();i++){
             boolean found = false;
-            for(int j =0;j<t.length();j++){
+            for(int j =i;j<t.length();j++){
                 if(s.charAt(i) == t.charAt(j)){
                     if((i != 0 && indexArr[i-1] < j) || i == 0){
                         found = true;
