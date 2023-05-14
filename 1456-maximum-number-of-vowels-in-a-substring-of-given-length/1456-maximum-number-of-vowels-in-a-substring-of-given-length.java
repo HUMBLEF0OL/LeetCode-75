5a6c4dbad1ace965a902330a0efc1b9ebe5d0b51
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-05-14 14:15:06 +0530

    Time: 43 ms (16.51%), Space: 43.6 MB (5.15%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,34 +1,22 @@
 class Solution {
-    private boolean checkVowel(char ch){
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch=='o' || ch == 'u') return true;
-        return false;
-    }
     public int maxVowels(String s, int k) {
-        int result = 0;
-        int currentCount = 0;
-        StringBuilder currentString = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            currentString.append(s.charAt(i));
-            // System.out.println(currentString);
-            if (i < k) {
-                if (checkVowel(currentChar)) {
-                    result++;
-                    currentCount++;
-                }
-            } else {
-                char startingChar = currentString.charAt(0);
-                if (checkVowel(startingChar)) {
-                    currentCount--;
-                }
-                currentString.deleteCharAt(0);
-                if (checkVowel(currentChar)) {
-                    currentCount++;
-                }
-                result = Math.max(currentCount, result);
-
-            }
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+        
+        // Build the window of size k, count the number of vowels it contains.
+        int count = 0;
+        for (int i = 0; i < k; i++) {
+            count += vowels.contains(s.charAt(i)) ? 1 : 0;
         }
-        return result;
+        int answer = count;
+        
+        // Slide the window to the right, focus on the added character and the
+        // removed character and update "count". Record the largest "count".
+        for (int i = k; i < s.length(); i++) {
+            count += vowels.contains(s.charAt(i)) ? 1 : 0;
+            count -= vowels.contains(s.charAt(i - k)) ? 1 : 0;
+            answer = Math.max(answer, count);
+        }
+        
+        return answer;
     }
 }
