14612238d721c3f1782b97ff25b85dab324eeb57
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-05-15 19:53:27 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (84.21%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -5,8 +5,6 @@ class Solution {
         for(int i =0;i<gain.length;i++){
             currentAltitude += gain[i];
             result = Math.max(result,currentAltitude);
-            System.out.println(result + " "+ currentAltitude + " "+ gain[i]);
-            // if(currentAltitude < gain[i]) currentAltitude = gain[i];
         }
         return result < 0 ? 0 : result;
     }
