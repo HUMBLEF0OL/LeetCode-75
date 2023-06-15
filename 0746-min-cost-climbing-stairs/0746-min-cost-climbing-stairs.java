33b79440d7088d525c096a16326eece1ff540fab
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-06-15 08:15:40 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (17.37%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -14,13 +14,23 @@ class Solution {
         return dp[n];
     }
     public int minCostClimbingStairs(int[] cost) {
-        int dp[] = new int[cost.length];
-        dp[0] = cost[0];
-        dp[1]  = cost[1];
-        for(int i =2;i<cost.length;i++){
-            dp[i] = cost[i] + Math.min(dp[i-1],dp[i-2]);
-        }
-        return Math.min(dp[dp.length-1],dp[dp.length-2]);
+        // int dp[] = new int[cost.length];
+        // dp[0] = cost[0];
+        // dp[1]  = cost[1];
+        // for(int i =2;i<cost.length;i++){
+        //     dp[i] = cost[i] + Math.min(dp[i-1],dp[i-2]);
+        // }
+        // return Math.min(dp[dp.length-1],dp[dp.length-2]);
         // return Math.min(rHelper(cost,dp,cost.length-1),rHelper(cost,dp,cost.length-2));
+        
+        int first = cost[0];
+        int second = cost[1];
+        if(cost.length <=2)return Math.min(first,second);
+        for(int i=2;i<cost.length;i++){
+            int current = cost[i] + Math.min(first,second);
+            first = second;
+            second = current;
+        }
+        return Math.min(first,second);
     }
 }
